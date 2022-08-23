356667be7e45b6a56507666fafd03fe1772881c2
xavierfernando <xavierbr@gmail.com>
2022-08-23 14:25:21 -0300

    termino aula 16 angular

--- crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { delay, first, tap } from 'rxjs';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -16,7 +16,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(5000),
+      delay(1000),
       tap(courses => console.log(courses))
 
     );
