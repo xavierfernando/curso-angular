abc5816c5d882c97ffcbc9b63c58e9b44c14580d
xavierfernando <xavierbr@gmail.com>
2022-08-22 23:41:08 -0300

    termino da aula 15

--- crud-angular-spring/crud-spring/src/main/java/br/com/xavierfernando/crudspring/CrudSpringApplication.java
+++ crud-angular-spring/crud-spring/src/main/java/br/com/xavierfernando/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package br.com.xavierfernando.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.xavierfernando.crudspring.model.Course;
+import br.com.xavierfernando.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -9,5 +14,22 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+			Course d = new Course();
+			d.setName("React");
+			d.setCategory("front-end");
+			courseRepository.save(d);
+		
+		};
+	}
+
 
 }
--- crud-angular-spring/crud-spring/src/main/java/br/com/xavierfernando/crudspring/controller/CourseController.java
+++ crud-angular-spring/crud-spring/src/main/java/br/com/xavierfernando/crudspring/controller/CourseController.java
@@ -7,13 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.xavierfernando.crudspring.model.Course;
+import br.com.xavierfernando.crudspring.repository.CourseRepository;
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
     
+    private final CourseRepository courseRepository;
+
+    
+
+
     @GetMapping
     public List<Course> list() {
-        return null;
+        return courseRepository.findAll();
     }
 }
